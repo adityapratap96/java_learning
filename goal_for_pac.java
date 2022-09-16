b2cd66248364a54a9af2e1703ed2c190ccba878b
aditya <112940657+adityapratap96@users.noreply.github.com>
2022-09-16 16:50:45 +0530

    to cheak that we can complete goal of desired kg packaging from availabe pack of 5kg and 1 kg

---
+++ goal_for_pac.java
@@ -0,0 +1,32 @@
+package good_ques;
+
+import javax.sound.sampled.SourceDataLine;
+
+
+public class goal_for_pac {
+    public static boolean canPack(int bigCount,int smallCount ,int goal)
+{
+    if(bigCount<0||smallCount<0||goal<0)
+    {
+        return false;
+    }
+    int sum =0;
+    
+    for(int i=0;i<=bigCount;i++)
+    {
+        for(int j=0;j<=smallCount;j++)
+        {
+            sum = (5*i)+(1*j);
+            if(sum==goal)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+    public static void main(String[] args)
+    {    
+        System.out.println(canPack(4,3,13));
+    }
+}
